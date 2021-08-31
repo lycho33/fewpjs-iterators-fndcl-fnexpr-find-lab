d05e9aa9c4f5dfc38b60ae7814a03d93ba9f7208
lycho33 <cho.lyds@gmail.com>
2021-08-31 16:55:49 -0400

    commit tree bb4abc43f12c55fac613eba967b90d73120965ce

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const record = [
+   {year: "2018", result: "L"},
+   {year: "2017", result: "W"},
+   {year: "2016", result: "N/A"}
+   //...
+ ]
+// function superbowlWin(record) {
+//    record.find((record) => {
+//       if (record.result === 'W') {
+//          return result.year
+//       } else {
+//          return undefined
+//       }
+//    })
+// }
 
+superbowlWin = (record) => {
+   let result = record.find( record => record.result === "W" )
+   return !!result ? result.year : undefined
+ }
